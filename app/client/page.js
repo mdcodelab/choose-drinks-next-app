c3ff2d372d2dbb7f574cd825a43eebf789fae718
miha11-hash <d_mihaela@msn.com>
2024-06-11 19:56:21 +0300

    Nvbar

--- app/client/page.js
+++ app/client/page.js
@@ -1,9 +1,12 @@
-import React from 'react'
+"use client";
+import React from 'react';
 
 function page() {
+  const[count, setCount]=React.useState(0);
   return (
     <div>
-      Client
+      <h1 className="text-3xl mx-10">{count}</h1>
+      <button className="btn  btn-primary" onClick={()=>setCount(count+1)}>Increase</button>
     </div>
   )
 }
--- app/page.js
+++ app/page.js
@@ -2,10 +2,11 @@ import React from 'react';
 import Link from 'next/link';
 
 function HomePage() {
+  console.log("home page");
   return (
     <div>
       <h1 className="text-7xl">Home Page</h1>
-      <Link href="/about">About Page</Link>
+      <Link href="/client" className="btn btn-accent">Get Started</Link>
     </div>
   )
 }
