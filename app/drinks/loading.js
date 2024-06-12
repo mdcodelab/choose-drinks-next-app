165e1b92a5556e9c60ec6738936a4a6161933d17
miha11-hash <d_mihaela@msn.com>
2024-06-12 15:16:11 +0300

    loading component

---
+++ app/drinks/loading.js
@@ -0,0 +1,5 @@
+const loading = ()=> {
+    return <span className="loading">Loading</span>
+}
+
+export default loading;
--- app/drinks/page.js
+++ app/drinks/page.js
@@ -1,11 +1,16 @@
-import React from 'react'
+
 const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a";
 
-async function DrinksPage () {
-    const response = await fetch(url);
+const fetchDrinks = async ()=>{
+  await new Promise((resolve)=> setTimeout(resolve, 1000));
+const response = await fetch(url);
     const data= await response.json();
-    console.log(data);
-  
+    return data;
+}
+
+async function DrinksPage () {
+    const data = await fetchDrinks();
+    console.log(data)  
 
   return (
     <div>
