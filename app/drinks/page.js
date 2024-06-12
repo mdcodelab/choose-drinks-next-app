8549650f6412abfebfa202952eee935b7517f02b
miha11-hash <d_mihaela@msn.com>
2024-06-12 14:45:04 +0300

    fetch drinks

--- app/drinks/page.js
+++ app/drinks/page.js
@@ -1,6 +1,12 @@
 import React from 'react'
+const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a";
 
-function DrinksPage () {
+async function DrinksPage () {
+    const response = await fetch(url);
+    const data= await response.json();
+    console.log(data);
+  
+
   return (
     <div>
       Drinks Page
