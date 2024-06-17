165b78dff53d1203f0f5f50b09d63dea4dc790fd
miha11-hash <d_mihaela@msn.com>
2024-06-17 18:54:14 +0300

    Prisma CRUD

--- app/prisma-example/page.js
+++ app/prisma-example/page.js
@@ -1,11 +1,63 @@
-import React from 'react'
+import prisma from "../../utils/db";
 
-function PrismaExample() {
+const prismaHandlers = async () => {
+  // delete all existing tasks
+  await prisma.task.deleteMany({});
+
+  // create 1 task
+  await prisma.task.create({
+    data: {
+      content: "wake up",
+    },
+  });
+
+  // obtain all tasks
+  const allTasks = await prisma.task.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  // By ID
+  const task = await prisma.task.findUnique({
+    where: {
+      id: id,
+    },
+  });
+
+///update a task
+  const updateTask = await prisma.task.update({
+    where: {
+      id: id,
+    },
+    data: {
+      content: "updated task",
+    },
+  });
+
+  //delete a single record
+const deleteTask = await prisma.task.delete({
+  where: {
+    id: id,
+  },
+});
+
+  return allTasks;
+};
+
+
+
+async function PrismaExample() {
+  const allTasks = await prismaHandlers();
   return (
     <div>
-      Prisma-Example
+      <h1>Prisma Example</h1>
+      {allTasks.map((task) => (
+        <h2 key={task.id} className="text-xl py-2">
+          {task.content}
+        </h2>
+      ))}
     </div>
-  )
+  );
 }
 
-export default PrismaExample
+export default PrismaExample;
