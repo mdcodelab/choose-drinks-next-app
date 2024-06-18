b6da28f26e6616a6c261fa93a1c909d6242c6a38
miha11-hash <d_mihaela@msn.com>
2024-06-18 19:50:07 +0300

    First Action - form action - create task

--- components/TaskForm.jsx
+++ components/TaskForm.jsx
@@ -1,13 +1,32 @@
 
+const createTask = async(formData) => {
+'use server'
+const content = formData.get("content");
+console.log(content);
+await prisma.task.create({
+  data: {
+    content
+  }
+})
+revalidatePath("/tasks");
+}
+
 
 function TaskForm() {
   return (
-    <form>
-    <input type="text" className="border-2 border-color: rgb(0,0,0) rounded p-2"
-    placeholder="Create task..."></input> 
-    <button type="submit" className="btn bg-blue-700">Create Task</button>
+    <form className="flex w-full" action={createTask}>
+      <input
+        type="text"
+        className="input input-bordered flex-1 w-full rounded p-2 mr-2"
+        placeholder="Create task..."
+        name="content"
+        required
+      />
+      <button type="submit" className="btn btn-primary">
+        Create Task
+      </button>
     </form>
-  )
+  );
 }
 
-export default TaskForm
+export default TaskForm;
