af2ddd45152556d69e25e723e63595be09227aac
miha11-hash <d_mihaela@msn.com>
2024-07-04 00:45:11 +0300

    zod library

--- utils/actions.js
+++ utils/actions.js
@@ -3,6 +3,7 @@
 import prisma from "./db";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
+import {z} from "zod";
 
 export const getAllTasks = async () => {
     return await prisma.task.findMany({
@@ -53,8 +54,12 @@ redirect("/tasks");
 export const createTaskCustom = async (prevState, formData) => {
   await new Promise((resolve) => setTimeout(resolve, 2000));
   const content = formData.get("content");
-  // some validation here
+  const Task = z.object({
+    content: z.string().min(5)
+  })
+  
   try {
+    Task.parse({content});
     await prisma.task.create({
       data: {
         content,
@@ -64,8 +69,8 @@ export const createTaskCustom = async (prevState, formData) => {
     revalidatePath("/tasks");
     return { message: "success!!!" };
   } catch (error) {
-    // can't return error
-    return { message: "error..." };
+    console.log(error);
+    return { message: "Error..." };
   }
 };
 
